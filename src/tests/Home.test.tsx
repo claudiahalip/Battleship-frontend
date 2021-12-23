5f21d3b9ed8d9ff0a1ac341414e099c0d56cc9a0
claudiahalip <claudia4ro@yahoo.com>
2021-12-23 10:38:57 -0600

    change test to use Jest mocks

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import { render, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Home from '../components/Home';
+import 'jest';
+import axios from 'axios';
 
-const server = setupServer(
-  rest.get('http://localhost:8080/', (req, res, ctx) => res(ctx.json('Welcome to Battleship'))),
-);
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+test("reterns a welcome message", () => {
+    const response = {data: "Welcome to Battleship"};
+    expect(mockedAxios.get.mockResolvedValue(response)).toBeInTheDocument
+   
+})
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
 
-test('loads and displays welcome message', async () => {
-  render(<Home />);
-  const divElement = await waitFor(() => screen.getByText(/Welcome to Battleship/i));
-  expect(divElement).toBeInTheDocument();
-});
