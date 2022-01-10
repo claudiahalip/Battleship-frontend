200fe04f018b767c86e60f02086d7ef20168326e
Lane Harris <lrharris215@gmail.com>
2022-01-10 16:28:40 -0500

    Start now button now makes ship list appear

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Instructions from './Instructions';
 import ShipsContainer from './ShipsContainer';
 
 const Sidebar: React.FC = function () {
+    const [shipsVisible, setShipsVisible] = useState(false);
+
+    const handleStartNow = () => {
+        setShipsVisible(true);
+    };
+
     return (
         <div>
-            <Instructions />
-            <ShipsContainer />
-            <button className="start-now-button" type="button">
-                Start now
-            </button>
+            {shipsVisible ? <ShipsContainer /> : <Instructions />}
+
+            {shipsVisible ? (
+                <button className="start-game-button" type="button">
+                    Start Game
+                </button>
+            ) : (
+                <button className="start-now-button" type="button" onClick={handleStartNow}>
+                    Start now
+                </button>
+            )}
         </div>
     );
 };
